ae55cd2011ee4f548f81e9668ed1f9c678f52860
Mohamed Aboskien <m7mdaboskien@gmail.com>
2026-02-14 10:57:44 +0200

    Final 2

--- wp-private/password_hasher.php
+++ wp-private/password_hasher.php
@@ -12,8 +12,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Password Hasher</title>
-    <link href="https:
-    <link href="https:
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         body {
